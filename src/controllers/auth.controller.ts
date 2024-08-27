c911caef5025a20a522f519d6af1e3b0c0584b47
Ahbiemarc <ahebiemarc22@gmail.com>
2024-08-27 14:21:40 +0100

    I add the login and logout actions

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma";
 
 
@@ -28,16 +29,38 @@ export const register = async (req: Request, res: Response) => {
    }
 }
 
-export const login = (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response) => {
     // Code to authenticate | db operations
     const {username, password} = req.body
 
     try {
         // check if the user exists
 
+        const user = await prisma.user.findUnique({where: {username: username}});
+
+        if (!user) return res.status(401).json({message: 'User not found!'});
+
         // check if the the password is valid
 
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) return res.status(401).json({message: 'Invalid password!'});
+
         // generate a cookie token for the user
+        
+        //res.setHeader('Set-Cookie', "test=" + "myVAlue").json({message: "success"});
+
+        const age = 1000 * 60 * 60 * 24 * 7; // a one week
+
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET_KEY, {expiresIn: age});
+
+        res.cookie('HAUS-AUTH', token, 
+            {
+                httpOnly: true,
+                //secure: true, // that's production
+                maxAge:age
+            }).status(200).json({message: "authentication successful!"});
+
     } catch (error) {
         console.log(error);
         res.status(500).json({message: 'Failed to authenticate user!'});
@@ -46,6 +69,5 @@ export const login = (req: Request, res: Response) => {
 }
 export const logout = (req: Request, res: Response) =>{
     // Code to log out a user goes here
-    res.send('User logged out successfully');
-    // Code to log out | db operations
+    res.clearCookie("HAUS-AUTH").status(200).json({message: "Logout successful"})
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import postRoute from './routes/post.route';
 import authRoute from './routes/auth.route';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-
 // middleware
 app.use(express.json());
-
+app.use(cookieParser());
 
 // app routes
 app.use('/api/post', postRoute);
